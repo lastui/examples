82016b3f6ad0e2b8c3f02fb556a35c0fb4c02884
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-04-07 00:24:54 +0200

    configurable context

--- spa/src/index.js
+++ spa/src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Main } from '@lastui/rocker/runtime';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Main } from "@lastui/rocker/runtime";
+import { getContext } from "./service";
 
-window.addEventListener('load', (event) => {
-	ReactDOM.render(<Main />, document.getElementById('mount'))
-})
+window.addEventListener("load", (event) => {
+	const main = <Main fetchContext={getContext} />;
+	ReactDOM.render(main, document.getElementById("mount"));
+});
