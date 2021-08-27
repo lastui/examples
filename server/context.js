e9512785ef1e3f58dd13605ffd70e3c558e838ca
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-08-27 08:21:41 +0200

    check digest of program to introduce auto-update

---
+++ server/context.js
@@ -0,0 +1,167 @@
+const forge = require('node-forge');
+const fs = require('fs');
+const path = require('path');
+
+
+function sha256(file) {
+	const data = fs.readFileSync(path.join(__dirname, '..', file), 'utf8')
+	const md = forge.md.sha256.create();
+	md.update(data);
+	return md.digest().toHex()
+}
+
+function hash_programs(context) {
+
+	return {
+		entrypoint: context.entrypoint,
+		available: context.available.map(function(item) {
+			return {
+				id: item.id,
+				program: item.program ? {
+					url: item.program.url,
+					sha256: sha256(item.program.url),
+				} : undefined,
+				locales: item.locales,
+				meta: item.meta,
+			}
+		})
+	}
+}
+
+let context_A = {
+	available: [
+		{
+			id: 'broken',
+			program: {
+				url: '/examples/broken/build/main.min.js'
+			},
+			locales: {
+				'en-US': '/examples/broken/build/messages/en-US.json',
+			},
+			meta: {},
+		},
+		{
+			id: 'simple',
+			program: {
+				url: '/examples/simple/build/main.min.js',
+			},
+			locales: {
+				'en-US': '/examples/simple/build/messages/en-US.json',
+			},
+			meta: {},
+		},
+		{
+			id: 'localisation',
+			program: {
+				url: '/examples/localisation/build/main.min.js'
+			},
+			locales: {
+				'en-US': '/examples/localisation/build/messages/en-US.json',
+				'cs-CZ': '/examples/localisation/build/messages/cs-CZ.json',
+			},
+			meta: {},
+		},
+		{
+			id: 'layout',
+			program: {
+				url: '/examples/layout/build/main.min.js'
+			},
+			locales: {
+				'en-US': '/examples/layout/build/messages/en-US.json',
+			},
+			meta: {
+				info: 'Meta data info',
+			},
+		},
+		{
+			id: 'routing',
+			program: {
+				url: '/examples/routing/build/main.min.js'
+			},
+			locales: {
+				'en-US': '/examples/routing/build/messages/en-US.json',
+			},
+			meta: {},
+		},
+		{
+			id: 'subrouting',
+			program: {
+				url: '/examples/subrouting/build/main.min.js',
+			},
+			locales: {},
+			meta: {},
+		},
+	],
+	entrypoint: 'layout',
+}
+
+let context_B = {
+	available: [
+		{
+			id: 'localisation',
+			program: {
+				url: '/examples/localisation/build/main.min.js'
+			},
+			locales: {
+				'en-US': '/examples/localisation/build/messages/en-US.json',
+				'cs-CZ': '/examples/localisation/build/messages/cs-CZ.json',
+			},
+			meta: {},
+		},
+		{
+			id: 'layout',
+			program: {
+				url: '/examples/layout/build/main.min.js'
+			},
+			locales: {
+				'en-US': '/examples/layout/build/messages/en-US.json',
+			},
+			meta: {
+				info: 'Meta data info',
+			},
+		}
+	],
+	entrypoint: 'layout',
+}
+
+let context_C = {
+	available: [
+		{
+			id: 'layout',
+			program: {
+				url: '/examples/layout/build/main.min.js'
+			},
+			locales: {
+				'en-US': '/examples/layout/build/messages/en-US.json',
+			},
+			meta: {
+				info: 'Meta data info',
+			},
+		},
+		{
+			id: 'routing',
+			program: {
+				url: '/examples/routing/build/main.min.js'
+			},
+			locales: {
+				'en-US': '/examples/routing/build/messages/en-US.json',
+			},
+			meta: {},
+		},
+		{
+			id: 'subrouting',
+			program: {
+				url: '/examples/subrouting/build/main.min.js'
+			},
+			locales: {},
+			meta: {},
+		},
+	],
+	entrypoint: 'layout',
+}
+
+module.exports = {
+	A: hash_programs(context_A),
+	B: hash_programs(context_B),
+	C: hash_programs(context_C),
+}
--- server/server.js
+++ server/server.js
@@ -1,118 +1,11 @@
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
+const contexts = require('./context');
 
 let start_time = Date.now()
 
-let context_A = {
-	available: [
-		{
-			id: 'broken',
-			program: '/examples/broken/build/main.min.js',
-			locales: {
-				'en-US': '/examples/broken/build/messages/en-US.json',
-			},
-			meta: {},
-		},
-		{
-			id: 'simple',
-			program: '/examples/simple/build/main.min.js',
-			locales: {
-				'en-US': '/examples/simple/build/messages/en-US.json',
-			},
-			meta: {},
-		},
-		{
-			id: 'localisation',
-			program: '/examples/localisation/build/main.min.js',
-			locales: {
-				'en-US': '/examples/localisation/build/messages/en-US.json',
-				'cs-CZ': '/examples/localisation/build/messages/cs-CZ.json',
-			},
-			meta: {},
-		},
-		{
-			id: 'layout',
-			program: '/examples/layout/build/main.min.js',
-			locales: {
-				'en-US': '/examples/layout/build/messages/en-US.json',
-			},
-			meta: {
-				info: 'Meta data info',
-			},
-		},
-		{
-			id: 'routing',
-			program: '/examples/routing/build/main.min.js',
-			locales: {
-				'en-US': '/examples/routing/build/messages/en-US.json',
-			},
-			meta: {},
-		},
-		{
-			id: 'subrouting',
-			program: '/examples/subrouting/build/main.min.js',
-			locales: {},
-			meta: {},
-		},
-	],
-	entrypoint: 'layout',
-}
 
-let context_B = {
-	available: [
-		{
-			id: 'localisation',
-			program: '/examples/localisation/build/main.min.js',
-			locales: {
-				'en-US': '/examples/localisation/build/messages/en-US.json',
-				'cs-CZ': '/examples/localisation/build/messages/cs-CZ.json',
-			},
-			meta: {},
-		},
-		{
-			id: 'layout',
-			program: '/examples/layout/build/main.min.js',
-			locales: {
-				'en-US': '/examples/layout/build/messages/en-US.json',
-			},
-			meta: {
-				info: 'Meta data info',
-			},
-		}
-	],
-	entrypoint: 'layout',
-}
-
-let context_C = {
-	available: [
-		{
-			id: 'layout',
-			program: '/examples/layout/build/main.min.js',
-			locales: {
-				'en-US': '/examples/layout/build/messages/en-US.json',
-			},
-			meta: {
-				info: 'Meta data info',
-			},
-		},
-		{
-			id: 'routing',
-			program: '/examples/routing/build/main.min.js',
-			locales: {
-				'en-US': '/examples/routing/build/messages/en-US.json',
-			},
-			meta: {},
-		},
-		{
-			id: 'subrouting',
-			program: '/examples/subrouting/build/main.min.js',
-			locales: {},
-			meta: {},
-		},
-	],
-	entrypoint: 'layout',
-}
 
 async function index(req, res) {
 	const filePath = path.join(__dirname, '../spa/build/index.html');
@@ -140,7 +33,7 @@ async function asset(req, res) {
 }
 
 async function context(req, res) {
-	return res.json(context_A)
+	return res.json(contexts.A)
 
 	/*
 	const now = Date.now()
@@ -149,7 +42,7 @@ async function context(req, res) {
 		start_time = now
 		elapsed = 0
 	}
-	return res.json(elapsed === 0 ? context_B : context_A)
+	return res.json(elapsed === 0 ? contexts.B : contexts.A)
 	*/
 }
 
