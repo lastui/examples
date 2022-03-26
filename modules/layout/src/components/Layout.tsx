e38d2b90665b63e4d987ffc3642ac103eafacb15
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-03-26 12:55:00 +0100

    upgraded rocker to version with lazy loaded locales

--- modules/layout/src/components/Layout.tsx
+++ modules/layout/src/components/Layout.tsx
@@ -4,7 +4,6 @@ import { Module } from '@lastui/rocker/platform';
 
 const Layout = (props: any) => (
 	<React.Fragment>
-		{"layout here"}
 		<Module name="self-state-ioc" />
 		<Module name="failing-saga-boundary" />
 		<Module name="localisation" />
