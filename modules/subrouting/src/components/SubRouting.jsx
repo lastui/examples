79051f0eef91df36317773c7ea19bcb9b7e99bfc
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-05-01 01:01:40 +0200

    new shared data contract approach

--- modules/subrouting/src/components/SubRouting.jsx
+++ modules/subrouting/src/components/SubRouting.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { Route, useRouteMatch, useLocation } from "@lastui/rocker/platform";
+import { Route, useRouteMatch, useLocation, actions } from "@lastui/rocker/platform";
 
-const Main = () => <div>Main here</div>;
+const Main = () => {
+  const sharedState = useSelector((state) => state.shared);
+  return (
+    <div>
+      <div>
+        <p>Global shared state is</p>
+        <code>{JSON.stringify(sharedState.global, null, 4)}</code>
+      </div>
+      <div>
+        <p>Localised shared state is</p>
+        <code>{JSON.stringify(sharedState.local, null, 4)}</code>
+      </div>
+    </div>
+  );
+};
+
 const Sub = () => <div>Sub here and my match is</div>;
 
 const SubRouting = () => {
+  const dispatch = useDispatch();
   const match = useRouteMatch("/:part");
+
+  const dispatchLocalShared = React.useCallback(() => dispatch(actions.setLocalShared({ 'info': 'here' })), [dispatch]);
+  const dispatchGlobalShared = React.useCallback(() => dispatch(actions.setGlobalShared({ 'routing': 'exits' })), [dispatch]);
+
+  React.useEffect(() => {
+    dispatchLocalShared();
+    dispatchGlobalShared();
+  }, [dispatchLocalShared, dispatchGlobalShared]);
 
   return (
     <React.Fragment>
