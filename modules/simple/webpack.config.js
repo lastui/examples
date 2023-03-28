9b9a1eee31ed9b548a486cd4ebc0361ab974b570
Jan Cajthaml <jan.cajthaml@gmail.com>
2023-03-28 20:11:59 +0200

    more stable polyfills

--- modules/simple/webpack.config.js
+++ modules/simple/webpack.config.js
@@ -1,9 +1,7 @@
 const config = require("@lastui/rocker/webpack/config/module");
 
 config.entry = {
-  simple: [
-    "./src/index.js"
-  ],
+  simple: ["./src/index.js"],
 };
 
 module.exports = config;
