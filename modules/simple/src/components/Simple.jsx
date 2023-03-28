020172908b4fe94667b852808f2cbbac07976c9d
Jan Cajthaml <jan.cajthaml@gmail.com>
2023-03-28 20:11:15 +0200

    more stable polyfills

--- modules/simple/src/components/Simple.jsx
+++ modules/simple/src/components/Simple.jsx
@@ -6,6 +6,18 @@ import { Module } from "@lastui/rocker/platform";
 const Simple = (props) => {
   const count = useSelector((state) => state.ticker);
 
+  const object1 = {};
+
+  Object.defineProperties(object1, {
+    property1: {
+      value: 42,
+      writable: true
+    },
+    property2: {}
+  });
+
+  console.log(object1.property1);
+
   return (
     <div className="notification simple">
       <div>{`elapsed ${count}...`}</div>
--- modules/simple/webpack.config.js
+++ modules/simple/webpack.config.js
@@ -1,7 +1,9 @@
 const config = require("@lastui/rocker/webpack/config/module");
 
 config.entry = {
-  simple: ["./src/index.js"],
+  simple: [
+    "./src/index.js"
+  ],
 };
 
 module.exports = config;
